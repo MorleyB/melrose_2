ccc4895f1179270c622e96da51ec3548a37ec9d7
morleyb <morley@populr.me>
2013-09-09 19:46:28 -0500

    revised 3rd column for image assets including styles and slider, added new region in the logo area for ease of switching images

--- js/main.js
+++ js/main.js
@@ -6,7 +6,7 @@
     	$('.columnizer-row').each(function(idx, el) {
         // debugger
         half_width = ($(this).width()/2)
-        if ($(this).find('.asset-container').width() > half_width ) {
+        if ($(this).find('.asset-container').width() > half_width) {
           $(this).find('.asset-container').css('min-height', $(this).find('.asset').height() );
         } else {
           if ($(this).find('.asset').length > 1) {
@@ -17,34 +17,23 @@
   	});
   };
 
-  // uses jquery color plugin
-  setBackgroundColor = function() {
-    background = $('body')
-    color = $.Color(background.css('backgroundColor'))
-    assets = $('#pop').find('.asset').not('.asset.asset-type-text.asset-size-1').not('.asset.asset-type-imagegroup')
-    light_shade = color.lightness(.451).saturation(.28)
-    mid_shade = color.lightness(.23).saturation(.4)
-
-    assets.map(function(index){
-      if (index == 0) {
-        $(this).parent().css('background-color', light_shade);
-      } else if (index % 2 == 0) {
-        $(this).parent().css('background-color', light_shade);
-      } else {
-        $(this).parent().css('background-color', mid_shade);
-      }
-    })
+  appendImageClass = function() {
+    $($('.asset.asset-type-imagegroup.asset-size-3')[0]).children().find('.small-image').css('display', 'inline');
+    $($('.asset.asset-type-imagegroup.asset-size-3')[1]).children().find('.middle-image').css('display', 'inline');
+    $($('.asset.asset-type-imagegroup.asset-size-3')[2]).children().find('.small-image').css('display', 'inline');
   }
 
 
   $(document).on('pop-initialized', function() {
     $(window).on('resize', _.throttle(fixColumnHeights));
     $('.columnizer-row .asset').live('initialize', _.throttle(fixColumnHeights));
-    $('.columnizer-row .asset').live('initialize', setBackgroundColor);
+    $('.columnizer-row .asset').live('initialize', appendImageClass);
+    $('columnizer-row .asset').live('lightbox_enabled', function() {
+      $(this).css('zoom-glass', 'display:inline')
+    });
 
     $('.columnizer-row .asset').live('destroy', function() {
       $(this).find('.asset').css('min-height', '');
-      $('.columnizer-row .asset').live('destory', setBackgroundColor);
     });
   });
 
