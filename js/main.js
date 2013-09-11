75eb5f9efdc756c05f65f8faf2e2bdf976b15d3f
morleyb <morley@populr.me>
2013-09-11 13:20:23 -0500

    css to conditionally switch the slider for size 3 image assets

--- js/main.js
+++ js/main.js
@@ -17,20 +17,20 @@
   	});
   };
 
-  appendImageClass = function() {
-    $($('.asset.asset-type-imagegroup.asset-size-3')[0]).children().find('.small-image').css('display', 'inline');
-    $($('.asset.asset-type-imagegroup.asset-size-3')[1]).children().find('.middle-image').css('display', 'inline');
-    $($('.asset.asset-type-imagegroup.asset-size-3')[2]).children().find('.small-image').css('display', 'inline');
-  }
+  // appendImageClass = function() {
+  //   $($('.asset.asset-type-imagegroup.asset-size-3')[0]).children().find('.small-image').css('display', 'inline');
+  //   $($('.asset.asset-type-imagegroup.asset-size-3')[1]).children().find('.middle-image').css('display', 'inline');
+  //   $($('.asset.asset-type-imagegroup.asset-size-3')[2]).children().find('.small-image').css('display', 'inline');
+  // }
 
 
   $(document).on('pop-initialized', function() {
     $(window).on('resize', _.throttle(fixColumnHeights));
     $('.columnizer-row .asset').live('initialize', _.throttle(fixColumnHeights));
-    $('.columnizer-row .asset').live('initialize', appendImageClass);
-    $('columnizer-row .asset').live('lightbox_enabled', function() {
-      $(this).css('zoom-glass', 'display:inline')
-    });
+    // $('.columnizer-row .asset').live('initialize', appendImageClass);
+    // $('columnizer-row .asset').live('lightbox_enabled', function() {
+    //   $(this).css('zoom-glass', 'display:inline')
+    // });
 
     $('.columnizer-row .asset').live('destroy', function() {
       $(this).find('.asset').css('min-height', '');
